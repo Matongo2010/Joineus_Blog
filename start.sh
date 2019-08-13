e5779eefda72702a57aa50d8295d135d6138fe75
Matongo2010 <justineadriano638@gmail.com>
2019-08-13 09:12:20 +0300

    add readme file

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY="vince"
+export  MAIL_USERNAME="vinceobindi@gmail.com"
+export MAIL_PASSWORD="0717654230"
+export DATABASE_URL = 'postgresql+psycopg2://moringaschool:matongome9291@localhost/dre_blog'
+export
+python3.6 manage.py server 
